9f60dce8cb97aec8c6451d9a8aa841cad861f8ff
muthana <muthana@DESKTOP-BKN0CQS>
2025-04-30 18:07:20 +0400

    "Added printYOURNAME() method"

---
+++ project/src/main/java/com/mycompany/project/Project.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project;
+
+/**
+ *
+ * @author muthana
+ */
+public class Project {
+
+       public static void main(String[] args) {
+        System.out.println("Hello World!");
+        printYOURNAME(); // Call the new method
+    }
+
+    public static void printYOURNAME() {
+        System.out.println("My name is Muthana Rababah.");
+    }
+}
